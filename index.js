1ac06c7a2753dc8fba8d19ef616d2f08286b5ee2
tigerkaplan <husniyeerparun@gmail.com>
2023-12-24 11:32:53 +0000

    node module const added

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
+//node module
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+// team profile
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+
+
+// generateHTML file
+const generateHTML= require("./output/generateHTML");
+
+//team
+const Team = require("./lib/Team");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
