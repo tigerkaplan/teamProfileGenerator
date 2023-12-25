04a1b4580708e7427c80ad9fc662c14f6b95fb8d
tigerkaplan <husniyeerparun@gmail.com>
2023-12-25 01:03:12 +0000

    update variables

--- index.js
+++ index.js
@@ -2,20 +2,22 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-// team profile
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
-const inquirer = require("inquirer");
+// path
 const path = require("path");
-const fs = require("fs");
+
+//team
+const Team = require("./lib/Team");
+
+// team profile
+const Manager  = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Employee = require("./lib/Employee");
+const Intern   = require("./lib/Intern");
 
 
 // generateHTML file
 const generateHTML= require("./output/generateHTML");
 
-//team
-const Team = require("./lib/Team");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
