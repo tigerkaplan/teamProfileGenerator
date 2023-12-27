3b144972c81acd1a948d1fe76631082918f8f19e
tigerkaplan <husniyeerparun@gmail.com>
2023-12-27 23:38:58 +0000

    intern test created

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,19 +1,38 @@
 const Intern = require("../lib/Intern");
+const intern = new Intern("employee_1", "emp001", "employee_1@example.com", "UCLA");
 
-test("Can set school via constructor", () => {
-  const testValue = "UCLA";
-  const e = new Intern("Foo", 1, "test@test.com", testValue);
-  expect(e.school).toBe(testValue);
+test("Intern object properties", () => {
+  expect(intern.name).toBe("employee_1"); // Intern Name
+  expect(intern.id).toBe("emp001"); // Intern ID
+  expect(intern.email).toBe("employee_1@example.com"); // Intern Email
+  expect(intern.school).toBe("UCLA"); // Intern school
+});
+
+
+
+test("name via getName()", () => {
+  expect(intern.getName()).toBe("employee_1");
+});
+
+test("ID via getId()", () => {
+  expect(intern.getId()).toBe("emp001");
+});
+
+test("email via getEmail()", () => {
+  expect(intern.getEmail()).toBe("employee_1@example.com");
 });
 
 test("getRole() should return \"Intern\"", () => {
-  const testValue = "Intern";
-  const e = new Intern("Foo", 1, "test@test.com", "UCLA");
-  expect(e.getRole()).toBe(testValue);
+  expect(e.getRole()).toBe("Intern");
 });
 
+// test("Can set school via constructor", () => {
+//   const testValue = "UCLA";
+//   const e = new Intern("Foo", 1, "test@test.com", testValue);
+//   expect(e.school).toBe(testValue);
+// });
+
+
 test("Can get school via getSchool()", () => {
-  const testValue = "UCLA";
-  const e = new Intern("Foo", 1, "test@test.com", testValue);
-  expect(e.getSchool()).toBe(testValue);
+  expect(e.getSchool()).toBe("UCLA");
 });
